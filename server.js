f94ecb45f1a71c891ea61b6f40ffaf5bd7bc19ad
pietupai <pietupai@gmail.com>
2025-02-20 21:42:33 +0200

    Update server.js

--- server.js
+++ server.js
@@ -19,45 +19,24 @@ app.post('/api/webhook', async (req, res) => {
     const body = req.body;
     console.log('Webhook event received:', body);
 
-    // Fetch the updated content from the defined URLs
-    const responseUrls = [
-      'https://www.google.com/',
-      'https://www.iki.fi/',
-      'https://www.example.com/'
-    ];
+    // Fetch the updated content from response.txt with a timestamp to avoid caching
+    const timestamp = new Date().getTime();
+    const responseUrl = `https://raw.githubusercontent.com/pietupai/hae/main/response.txt?timestamp=${timestamp}`;
+    console.log(`Fetching from URL: ${responseUrl}`);
+    const response = await fetch(responseUrl, { headers: { 'Cache-Control': 'no-cache' } });
 
-    let responseData = '';
-
-    for (const url of responseUrls) {
-      console.log(`Fetching from URL: ${url}`);
-      const response = await fetch(url, { redirect: 'manual' }); // Handle redirects manually
-
-      if (response.status === 301 || response.status === 302) {
-        const redirectUrl = response.headers.get('location');
-        console.log(`Redirected to: ${redirectUrl}`);
-        const redirectedResponse = await fetch(redirectUrl);
-        const redirectedData = await redirectedResponse.text();
-
-        // Append redirected data to responseData
-        responseData += redirectedData;
-      } else {
-        const data = await response.text();
-
-        // Append fetched data to responseData
-        responseData += data;
-      }
-    }
+    const data = await response.text();
 
     // Save response data to response.txt
-    await fs.writeFile('response.txt', responseData);
+    await fs.writeFile('response.txt', data);
     console.log('Response data saved to response.txt');
 
     // Emit event with the updated content
-    console.log(`Emitting newWebhook event with data: ${responseData}`);
-    req.app.locals.eventEmitter.emit('newWebhook', responseData);
+    console.log(`Emitting newWebhook event with data: ${data}`);
+    req.app.locals.eventEmitter.emit('newWebhook', data);
     console.log('Event emitted successfully with data');
 
-    res.status(200).send(responseData);
+    res.status(200).send(data);
   } catch (error) {
     console.error('Error handling webhook:', error);
     res.status(500).send('Error handling webhook');
